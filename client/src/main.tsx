f7cfd4ca5f91c69bc5c80d4d6370cef083304ab9
youngseoKim <youngseo.kim@saltlux.com>
2026-05-15 13:32:04 +0900

    fix: 실행시 에러 수정

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Inject Analytics conditionally
+const analyticsEndpoint = import.meta.env.VITE_ANALYTICS_ENDPOINT;
+const websiteId = import.meta.env.VITE_ANALYTICS_WEBSITE_ID;
+
+if (analyticsEndpoint && websiteId && !analyticsEndpoint.includes('%VITE')) {
+  const script = document.createElement('script');
+  script.defer = true;
+  script.src = `${analyticsEndpoint}/umami`;
+  script.setAttribute('data-website-id', websiteId);
+  document.body.appendChild(script);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
